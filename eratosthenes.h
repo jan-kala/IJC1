70c39f85c04ac1853900cafc8ba6d1aa39d3b34c
Jan Kala <jankala96@gmail.com>
2017-03-14 09:59:12 +0100

    Pridavam hlavickovy soubor Erata a zip ve kterem je vse zabaleno (1.verze)

---
+++ eratosthenes.h
@@ -0,0 +1,18 @@
+//	eratosthenes.h
+//	Reseni IJC-DU1, priklad a), 14.3.2017
+//	Autor: Jan Kala, FIT
+//	Prelozeno: gcc 5.4.0 (Ubuntu 5.4.0-6ubuntu1~16.04.4)
+//	
+//	Hlavickovy soubor eratosthenes.h.
+//	
+#ifndef ERATOSTHENES_H
+#define ERATOSTHENES_H
+
+/**
+ * @brief      Eratosthenovo sito
+ *
+ * @param[in]  array  Bitove pole
+ */
+void Eratosthenes(bit_array_t array);
+
+#endif
